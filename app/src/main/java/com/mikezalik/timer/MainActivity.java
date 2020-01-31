b43c9bd9faff109d39abed6b6bfd7628bd1481a6
mikezalik <mike.zalik@gmail.com>
2020-01-31 10:50:59 -0800

    Adds Airhorn sound when timer is finished and updates button to change when timer is started.

--- app/src/main/java/com/mikezalik/timer/MainActivity.java
+++ app/src/main/java/com/mikezalik/timer/MainActivity.java
@@ -2,6 +2,7 @@ package com.mikezalik.timer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -15,8 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     TextView numTime;
     SeekBar seekBar;
+    Boolean counterIsActive = false;
+    Button timerStart;
 
     public void buttonClicked(View view) {
+
+        counterIsActive = true;
+        seekBar.setEnabled(false);
+        timerStart.setText("Stop Timer!");
+
        CountDownTimer countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000 + 100, 1000) {
            @Override
            public void onTick(long l) {
@@ -25,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
            @Override
            public void onFinish() {
-
+               MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+               mplayer.start();
            }
        }.start();
     }
@@ -37,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         String secondString = Integer.toString(seconds);
 
-        if (secondString.equals("0")) {
-            secondString = "00";
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
         }
 
         numTime.setText(String.format("%s:%s", Integer.toString(minutes), secondString));
@@ -51,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar = findViewById(R.id.seekBar);
         numTime = findViewById(R.id.numTime);
+        timerStart = findViewById(R.id.timerStart);
 
         seekBar.setMax(600);
         seekBar.setProgress(30);
