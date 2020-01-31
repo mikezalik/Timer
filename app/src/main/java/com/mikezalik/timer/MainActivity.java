84f284011c728e0a8e8e86ae6dd940a21a47dded
mikezalik <mike.zalik@gmail.com>
2020-01-30 17:41:26 -0800

    Adds seekBar max, progress and change listener. Temporarily fixes issues with constraints.

--- app/src/main/java/com/mikezalik/timer/MainActivity.java
+++ app/src/main/java/com/mikezalik/timer/MainActivity.java
@@ -3,6 +3,11 @@ package com.mikezalik.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        ImageView timerImage = findViewById(R.id.timerImage);
+        TextView numTime = findViewById(R.id.numTime);
+        Button timerStart = findViewById(R.id.timerStart);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
